48c3f0a0881b37147fbc53eecebb4623488de3b5
dylanhamada <dylan.hamada@gmail.com>
2023-11-07 16:08:58 -0500

    Create all-tweets Section
    - Create all-tweets section in index.html that includes username and profile picture, the tweet body, and a footer including the tweet date and functionality icons
    - Create all-tweets.css to style all-tweets section

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,9 +2,12 @@ $(document).ready(function() {
   const tweetInput = $("#tweet-text");
   const tweetCounter = $(".counter");
   
+  // adjust character counter as user types into textarea
   tweetInput.on("keyup", function(event) {
     let charsRemaining = 140 - $(this).val().length;
+
     tweetCounter.text(charsRemaining);
+    // change text color to red if user exceeds character limit
     if (charsRemaining < 0) {
       tweetCounter.addClass("invalid");
     } else {
