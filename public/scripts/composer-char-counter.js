6bad1cddb42c25871807853d445ddc49e9940f10
dylanhamada <dylan.hamada@gmail.com>
2023-11-08 15:31:22 -0500

    Modify variables in composer-char-counter.js to match jQuery variable naming convention

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
-  const tweetInput = $("#tweet-text");
-  const tweetCounter = $(".counter");
+  const $tweetInput = $("#tweet-text");
+  const $tweetCounter = $(".counter");
   
   // adjust character counter as user types into textarea
-  tweetInput.on("keyup", function(event) {
+  $tweetInput.on("keyup", function(event) {
     let charsRemaining = 140 - $(this).val().length;
 
-    tweetCounter.text(charsRemaining);
+    $tweetCounter.text(charsRemaining);
     // change text color to red if user exceeds character limit
     if (charsRemaining < 0) {
-      tweetCounter.addClass("invalid");
+      $tweetCounter.addClass("invalid");
     } else {
-      tweetCounter.removeClass("invalid");
+      $tweetCounter.removeClass("invalid");
     }
   })
 });
